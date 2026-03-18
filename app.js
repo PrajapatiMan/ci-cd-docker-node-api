90f8348ab07270128f1b33f9c52e63e10f36d799
Man Prajapati <manprajapati2162@gmail.com>
2026-03-18 10:59:28 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("🚀 CI/CD Docker API Running!");
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from API!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
